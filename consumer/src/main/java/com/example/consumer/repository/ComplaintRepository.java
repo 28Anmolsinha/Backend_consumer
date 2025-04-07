42422d53fbc844376980d8925ee5e75933164c25
28Anmolsinha <mailmeatanmol@gmail.com>
2025-04-07 16:36:50 +0530

    Add changes

--- consumer/src/main/java/com/example/consumer/repository/ComplaintRepository.java
+++ consumer/src/main/java/com/example/consumer/repository/ComplaintRepository.java
@@ -1,14 +1,12 @@
 package com.example.consumer.repository;
+
 import org.springframework.data.jpa.repository.Query;
-
-
 
 import com.example.consumer.entity.Complaint;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-
     @Query("SELECT MAX(CAST(c.id AS int)) FROM Complaint c")
     Integer findMaxId();
 }
